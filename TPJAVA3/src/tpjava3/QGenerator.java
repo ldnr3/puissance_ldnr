825a926c7c08c4abcf566e1fc3930473562346dc
fred <frederic.bras@free.fr>
2018-02-07 15:19:53 +0100

    Modifications de la classe abstraite QGenerator (cf tâche #1129)

--- TPJAVA3/src/tpjava3/QGenerator.java
+++ TPJAVA3/src/tpjava3/QGenerator.java
@@ -3,37 +3,44 @@ package tpjava3;
 
 public abstract class QGenerator {
     
-    private String question;
-    private String reponse;
-
-    public String getQuestion() {
-        return question;
-    }
-
-    public String getReponse() {
-        return reponse;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public void setReponse(String reponse) {
-        this.reponse = reponse;
-    }
+    /**
+     * La méthode getQuestion retourne l'énoncé de la question sous la forme 
+     * d'une chaîne de caractères
+     * 
+     * @return énoncé de la question
+     */
+    public abstract String getQuestion();
     
     /**
-     * Génére une nouvelle paire question réponse
+     * La méthode getResponse retourne la réponse associée à la question sous la 
+     * forme d'une châine de caractère
      * 
-     * IN   : niveau int (1|2)    permet de régler le niveau de la question générée
+     * @return réponse à la question posée
+     */
+    public abstract String getReponse();
+    
+    /**
+     * La méthode newQuestion génére une nouvelle question.
      * 
-     * OUT  : les attributs question et réponse contiennent la paire 
-     * (question, réponse) générée. La valeur de ces attributs privés
-     * est modifiée grâce aux mutateurs (setters) correspondant.
-     * 
-     * @param niveau
+     * @param niveau int {1|2}   niveau de la question à générer
      */
     
     public abstract void newQuestion(int niveau);
+    
+    /**
+     * génère aléatoirment un entier compris entre 0 et sup-1.
+     * 
+     * @param sup Borne supérieure des entiers générés aléatoirement
+     * 
+     * @return un entier compris entre 0 et sup -1
+     * 
+     */
+    
+    public int getRandomInteger(int sup) {
+        
+        int r = (int) Math.floor(sup*Math.random());
+        
+        return r;
+    }
     
 }
